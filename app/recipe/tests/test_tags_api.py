5520fd2fb13705b72f30c8a461df92ebaeb1ec16
leechoncaao <hathaykhongbanghayhat18@gmail.com>
2021-07-22 16:24:49 +0700

    Push for checking bugs

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -66,3 +66,25 @@ class PrivateTagsApiTest(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
+
+    def test_create_tag_successful(self):
+        """Test creating tag is success"""
+        payload = {'name': 'Simple'}
+
+        res = self.client.post(TAGS_URL, payload)
+
+        exist = Tag.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+
+        self.assertTrue(exist)
+        # self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+    def test_create_tag_invalid(self):
+        """Test creating tag with invalid payload"""
+        payload = {'name': ''}
+
+        res = self.client.post(TAGS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
